e53cc38bdafc4e02d759da633901f5dd74a14e8b
Chris Vasey <chrismvasey@gmail.com>
2022-11-10 20:08:45 +0000

    Revert "Fixed #2, customized footer content now fetched from proper backend URL"
    This reverts commit 557e34f2896a49ca694d5b30d06d18dc163ea983.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,5 +7,26 @@ use Laravel\Nova\Nova;
 
 class ServiceProvider extends BaseServiceProvider
 {
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::script('nova4-customizable-footer', $this->app->basePath("vendor/wdelfuego/nova4-customizable-footer/dist/nova4-customizable-footer.js"));
+        
+        Nova::router(['nova'], 'nova-vendor/wdelfuego/nova4-customizable-footer')
+            ->group($this->app->basePath('vendor/wdelfuego/nova4-customizable-footer/routes.php'));
+    }
 }
